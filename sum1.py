58749fbc39a20d14f8c286f138fb505c19617709
maple_mrzhao <mrzhao@mapleleaf.com>
2019-05-17 18:13:21 +0800

    some exercise

---
+++ sum1.py
@@ -0,0 +1,7 @@
+sum = 0
+x = 1
+while x < 100:
+    if x % 2 == 1:
+        sum = sum+x
+    x = x+1
+print(sum)
---
+++ test.py
@@ -0,0 +1,16 @@
+from student import Student
+
+# 测试:
+if Student.count != 0:
+    print('测试失败!')
+else:
+    bart = Student('Bart')
+    if Student.count != 1:
+        print('测试失败!')
+    else:
+        lisa = Student('Bart')
+        if Student.count != 2:
+            print('测试失败!')
+        else:
+            print('Students:', Student.count)
+            print('测试通过!')
